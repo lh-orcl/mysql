8108ba933ec9142abc5e2f5fdb853ef25ebf0bd7
lh-orcl <40397212+lh-orcl@users.noreply.github.com>
2018-07-19 11:40:56 +0100

    docker build.sh addition

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Exit if anything fails
+set -e
+
+DOCKER_RUN_REPO="lh-orcl"
+DOCKER_RUN_IMAGE="mysql"
+DOCKER_MYSQL_NET="client-net"
+
+docker build -t	$DOCKER_RUN_REPO/$DOCKER_RUN_IMAGE:latest .
+
+docker images
+docker run -di --name "$DOCKER_RUN_IMAGE" -p 3306:3306 -t "$DOCKER_RUN_REPO/$DOCKER_RUN_IMAGE"
+docker network connect $DOCKER_MYSQL_NET $DOCKER_RUN_IMAGE
+docker ps -a
